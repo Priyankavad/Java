57fe95cdc723dc26b8dc9904333959d9e203d14d
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-10-08 09:02:00 +0530

    Create create objects.java

---
+++ create objects.java
@@ -0,0 +1,13 @@
+public class CreateObject
+{  
+//constructor of the class    
+CreateObject()    
+{    
+System.out.println("Welcome to Github");    
+}    
+public static void main(String[] args)   
+{    
+//creating an object using new keyword   
+CreateObject obj = new CreateObject();   
+}    
+}
